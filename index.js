e278b05736969fffe55266fb8929c7d9ed50b8c1
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-04-09 15:15:32 +0300

    my index.js

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 
-
+const createLoginTracker = (user) => {
+  let failedAttempts = 0;
+  
+  return (attemptedPassword) => {
+    if (failedAttempts >= 3) {
+      return 'Account locked due to too many failed login attempts';
+    }
+    
+    if (attemptedPassword === user.password) {
+      return 'Login successful';
+    } else {
+      failedAttempts++;
+      return `Attempt ${failedAttempts}: Login failed`;
+    }
+  };
+};
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
